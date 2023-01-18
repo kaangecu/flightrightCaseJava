0912f936597acb34f2fcd304e75acec8fed6fb7c
Kaan Gecü <kaan.gecu@d-teknoloji.com.tr>
2023-01-18 04:21:13 +0300

    feature/refactor: csv reading logic changed now controller accepts MultipartFile instead of filePath and returns List. also added counting logic to service

--- src/main/java/com/flightright/flightrightCase/controller/VisitsCsvController.java
+++ src/main/java/com/flightright/flightrightCase/controller/VisitsCsvController.java
@@ -1,18 +1,30 @@
 package com.flightright.flightrightCase.controller;
 
+import com.flightright.flightrightCase.domain.dto.VisitSourceResponse;
 import com.flightright.flightrightCase.service.VisitsCsvService;
-import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/visits")
-public record VisitsCsvController(VisitsCsvService visitsCsvService) {
+@RequiredArgsConstructor
+public class VisitsCsvController {
+    private final VisitsCsvService visitsCsvService;
 
-    @PostMapping
-    public String getTotalVisits(@RequestParam String csvFilePath) {
-        visitsCsvService.csvReadder(csvFilePath);
-        return csvFilePath;
+    @PostMapping(consumes = "multipart/form-data")
+    public List<VisitSourceResponse> getTotalVisits(@RequestParam MultipartFile csvFile) throws ResponseStatusException {
+        return visitsCsvService.getVisitsFromCsv(csvFile)
+                .entrySet()
+                .stream()
+                .map(entry -> new VisitSourceResponse(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
     }
-
-
 }
--- src/main/java/com/flightright/flightrightCase/service/VisitsCsvService.java
+++ src/main/java/com/flightright/flightrightCase/service/VisitsCsvService.java
@@ -1,34 +1,53 @@
 package com.flightright.flightrightCase.service;
 
+import com.flightright.flightrightCase.domain.model.User;
+import com.flightright.flightrightCase.domain.model.Visit;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.*;
 
 @Service
 public class VisitsCsvService {
 
-    public void csvReadder(String filePath){
-
-        File csvFile = new File(filePath);
-
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] fields = line.split(",");
-                String email = fields[0];
-                String phone = fields[1];
-                String source = fields[2];
-
-                System.out.println(email+phone+source);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    private static Predicate<Visit> distinctByKey() {
+        Map<Object, Boolean> seen = new ConcurrentHashMap<>();
+        return t -> seen.putIfAbsent(t.user(), Boolean.TRUE) == null;
     }
 
+    public Map<String, Integer> getVisitsFromCsv(MultipartFile file) throws ResponseStatusException {
+        Map<String, Integer> sourceCounts;
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            // Skips first line since in csv files first line contains headers
+            Stream<String> lines = reader.lines().skip(1);
+
+            Map<User, String> firstVisits = lines.filter(line -> !line.startsWith(",") && !line.endsWith(","))
+                    .map(line -> line.split(","))
+                    .filter(columns -> !asList(columns).contains(""))
+                    .map(columns -> new Visit(new User(columns[0], columns[1]), columns[2]))
+                    .filter(distinctByKey())
+                    .collect(groupingBy(Visit::user, mapping(Visit::source, joining())));
+
+            sourceCounts = firstVisits.values().stream()
+                    .collect(groupingBy(identity(), summingInt(e -> 1)));
+
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "IO Exception");
+        }
+
+        return sourceCounts;
+    }
 }
