ddfc401b6b874988229ac29c8c4eee5194aa731d
Kaan Gecü <kaan.gecu@d-teknoloji.com.tr>
2023-01-18 04:22:38 +0300

    feature: cors policy work around

---
+++ src/main/java/com/flightright/flightrightCase/configuration/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.flightright.flightrightCase.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
